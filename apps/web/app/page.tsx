8490ff5ac39fbeb2f1158ab04f386cb7b946fe25
chandan123457 <chandankumarsang06@gmail.com>
2025-12-15 22:30:04 +0530

    update

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,20 @@
-import {prismaClient} from 'db/client';
+// import {prismaClient} from 'db/client';
+
+// export default async function Home() {
+
+//   const users = await prismaClient.user.findMany();
+//   return (
+//     <div>
+//       {JSON.stringify(users)}
+//     </div>
+//   )
+// }
 
 export default async function Home() {
+  const res = await fetch("http://15.207.14.18:8080/users", {
+    cache: "no-store"
+  });
+  const users = await res.json();
 
-  const users = await prismaClient.user.findMany();
-  return (
-    <div>
-      {JSON.stringify(users)}
-    </div>
-  )
+  return <div>{JSON.stringify(users)}</div>;
 }
